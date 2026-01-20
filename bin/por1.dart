057406783968450d183e3357fa0ae088ae5176f5
e <zo9q15@gmail.com>
2026-01-20 20:20:55 +0300

    print name

--- bin/por1.dart
+++ bin/por1.dart
@@ -1,12 +1,16 @@
 import 'package:por1/por1.dart' as por1;
 
 void main(List<String> arguments) {
+ person some =person(name: "mohammed");
+ some.printName();
   print('Hello world');
 }
 class person {
 String? name;
 int? age;
-person({name, age});
+person({this.name, this.age});
 
-
+void printName(){
+  print("name $name");
+}
 }
